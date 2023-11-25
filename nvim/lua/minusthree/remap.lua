e023b75b4813d7a7a25c3a14f534ad8ca93fbf63
Raza <raza.elahi@paypay-corp.co.jp>
2023-11-25 12:09:05 +0900

    Experimental: switch buffers with mm and mn.

--- nvim/lua/minusthree/remap.lua
+++ nvim/lua/minusthree/remap.lua
@@ -7,5 +7,5 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- vim.keymap.set('n', '<leader><leader>', function() vim.cmd('so') end)
-
+vim.keymap.set("n", "<leader>mm", vim.cmd.bnext)
+vim.keymap.set("n", "<leader>mn", vim.cmd.bprevious)
